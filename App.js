bd2da27fa0cc5383486a6ea3e7eb78c0d0278da4
Dmitriy <104869743+PrayForPleasures@users.noreply.github.com>
2024-08-24 15:20:56 +0300

    1st

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Image style={styles.main_image_fst} source={require('../pokedm/assets/pokemon.png')} />
+        <Image style={styles.main_image_scnd} source={require('../pokedm/assets/charmander.png')} />
+        <Pressable style={styles.main_button}
+          onPress={()=>console.log('press start')}>
+          <Text style={styles.main_button_text}>Начать игру</Text>
+        </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'whitesmoke',
+    justifyContent:'center',
+    alignItems:'center',
+    flex:1
   },
+  main_image_fst:{
+    resizeMode:'contain'
+  },
+  main_image_scnd:{
+    resizeMode:'contain'
+  },
+  main_button:{
+    paddingVertical: 20,
+    paddingHorizontal:20,
+    borderRadius:8,
+    backgroundColor:'yellow',
+    marginTop: 48,
+    borderColor: 'blue',
+    borderWidth:1,
+    shadowColor:'blue',
+    shadowOpacity:1,
+    shadowRadius:1,
+    shadowOffset:1
+  },
+  main_button_text:{
+    fontSize:18,
+    fontWeight:'bold',
+  }
+
 });
